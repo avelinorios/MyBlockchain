212cbe0bde9ecd5aef5a816a9f5c5e87fedc64f6
Avelino <avelino5872@gmail.com>
2022-10-20 20:24:03 +0200

    refactor: change test name

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -4,7 +4,7 @@ from infraestructure.blockchain import Blockchain
 
 class TestBlockchain(unittest.TestCase):
 
-    def test_last_block(self):
+    def test_genesis_block_creation(self):
         blockchain_to_test = Blockchain()
         self.assertEqual(len(blockchain_to_test.blocks), 1, "Should be 1")
 
