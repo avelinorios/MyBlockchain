2c573b19b76127e345ada511294eb4106807be9b
Avelino <avelino5872@gmail.com>
2022-11-20 19:55:18 +0100

    Add new transaction endpoint

--- api/api.py
+++ api/api.py
@@ -1,19 +1,20 @@
 from flask import request, Flask
 
 from infraestructure.blockchain import Blockchain
-from core.b_transaction import Transaction
 from infraestructure.converters.transaction_converter import TransactionConverter
 
-app = Flask(__name__)
 blockchain = Blockchain()
+
+app = Flask("api")
 
 
 @app.route('/new_transaction', methods=['POST'])
 def new_transaction():
-    request_body = request.form
-    request_transaction = TransactionConverter.from_json(request_body)
+    request_body = request.json
+    request_transaction = TransactionConverter().from_json(request_body)
     blockchain.add_transaction(request_transaction)
+    return "Created", 201
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True, port=9090)
